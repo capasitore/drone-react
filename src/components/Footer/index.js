2d88a37bb8dc6cb394264995298ebabbf75af08a
Rob BG <rob.bg@me.com>
2017-09-17 09:33:10 +0100

    Moved logo to top and added keyboard instructions

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './styles.css';
-import logo from './logo.svg';
 
 const Tagline = () => {
   return (
     <div className="footer">
       Built and designed by <a href="http://www.robbg.io">Rob BG</a> using React / Socket.io.&nbsp;
       <a href="https://github.com/robertbg/drone-react">Source code on Github</a>.
-      <img className="footer_logo" src={logo} alt="RBG Digital Ltd. Logo" />
     </div>
   );
 };
